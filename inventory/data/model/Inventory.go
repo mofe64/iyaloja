1e0e60d9fe2dea3eb375530e45b2651a48527ae1
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2022-03-20 07:14:16 +0100

    Inventory and Inventory Item models

--- inventory/data/model/Inventory.go
+++ inventory/data/model/Inventory.go
@@ -1,1 +1,16 @@
 package model
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type Inventory struct {
+	Id            primitive.ObjectID `json:"id" bson:"_id"`
+	InventoryTags []string           `json:"inventoryTags,omitempty"`
+	Description   string             `json:"description,omitempty"`
+	Name          string             `json:"name" validate:"required"`
+	DateCreated   time.Time          `json:"dateCreated"`
+	DateModified  time.Time          `json:"dateModified"`
+	ItemCount     int64              `json:"itemCount" default:"0"`
+}
--- inventory/data/model/Item.go
+++ inventory/data/model/Item.go
@@ -1,1 +1,20 @@
 package model
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type InventoryItem struct {
+	Id                        primitive.ObjectID `json:"id" bson:"_id"`
+	ItemName                  string             `json:"itemName"`
+	ItemCount                 int64              `json:"itemCount"`
+	Inventory                 primitive.ObjectID `json:"inventory"`
+	ItemNotificationThreshold int                `json:"itemNotificationThreshold,omitempty"`
+	InStock                   bool               `json:"inStock"`
+	CostPriceInfo             PriceInfo          `json:"costPriceInfo"`
+	SellPriceInfo             PriceInfo          `json:"sellPriceInfo"`
+}
+
+type PriceInfo struct {
+	BaseAmount int64  `json:"baseAmount"`
+	Unit       string `json:"unit"`
+	// TODO add currency enum
+}
