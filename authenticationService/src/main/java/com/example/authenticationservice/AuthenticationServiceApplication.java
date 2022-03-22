999cc3c964930da634e70ff8e3e2fb0327c0b92d
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2022-03-22 18:26:44 +0100

    token fix

--- authenticationService/src/main/java/com/example/authenticationservice/AuthenticationServiceApplication.java
+++ authenticationService/src/main/java/com/example/authenticationservice/AuthenticationServiceApplication.java
@@ -3,6 +3,7 @@ package com.example.authenticationservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
--- authenticationService/src/main/java/com/example/authenticationservice/config/AuthorizationServerConfig.java
+++ authenticationService/src/main/java/com/example/authenticationservice/config/AuthorizationServerConfig.java
@@ -48,4 +48,3 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     }
 }
-
