0ade8a0fd8a321a7a6c540239d6736b8e52c9c13
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2022-03-23 23:01:40 +0100

    setup userservice

---
+++ userservice/models/User.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import validator from 'validator';
+
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: [true, 'Please enter an email'],
+            unique: true,
+            lowercase: true,
+            validate: [validator.isEmail , 'please provide a valid email'],
+        },
+        password: {
+            type: String,
+            required: [true, 'please provide a password'],
+            minlength: 8,
+        },
+        passwordChangedAt: {
+            type: Date,
+        },
+        passwordResetToken: String,
+        passwordResetExpires: Date,
+        active: {
+            type: Boolean,
+            default: true,
+        },
+    }
+);
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
